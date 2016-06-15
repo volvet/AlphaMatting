eefe0b5c530c99bfaf322aa1900e2679fe5f9c6a
volvet <volvet2002@gmail.com>
2016-06-15 23:12:42 +0800

    copy shared matting code here

---
+++ AlphaMatting/AlphaMatting/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  AlphaMatting
+//
+//  Created by Volvet Zhang on 16/6/15.
+//  Copyright © 2016年 Volvet Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+#include "SharedMatting.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
